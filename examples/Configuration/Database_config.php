2adad93f90b2771622ac3524e7c3ee76b645493d
Iheb Elhabib <44964235+iheblhbib@users.noreply.github.com>
2021-02-10 10:22:24 +0100

    v1

---
+++ examples/Configuration/Database_config.php
@@ -0,0 +1,7 @@
+<?php
+
+//database_connection.php
+    $conn = new PDO('mysql:host=localhost;dbname=project_1', 'root', '');
+
+
+?>
---
+++ examples/Footer.php
@@ -0,0 +1,30 @@
+<footer class="footer">
+        <div class="container-fluid">
+          <nav class="float-left">
+            <ul>
+              <li>
+                <a href="https://www.xul-print.com.tn/">
+                  Xul Print
+                </a>
+              </li>
+              <li>
+                <a href="https://www.xul-print.com.tn/#nos-services">
+                À propos de nous
+                </a>
+              </li>
+              <li>
+                <a href="https://www.xul-print.com.tn/#contact">
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </nav>
+          <div class="copyright float-right">
+            &copy;
+            <script>
+              document.write(new Date().getFullYear())
+            </script>, Développer par 
+            <a href="https://www.xul-print.com.tn/" target="_blank">Xul Print</a>.
+          </div>
+        </div>
+      </footer>
